a46c96148aaf33fcdda9715ad0990b6c6b6a2124
Macumba45 <gonzalolovo@gmail.com>
2023-03-09 17:34:28 +0100

    style: dimensions category

--- src/componets/CardLocations/styles.ts
+++ src/componets/CardLocations/styles.ts
@@ -1,11 +1,33 @@
 import styled from 'styled-components';
+import { List } from '@mui/material';
 
 export const MainContainer = styled.div`
      display: flex;
      margin-top: 5rem;
-     /* flex-direction: column; */
+     flex-direction: column;
      flex-wrap: wrap;
-     margin-bottom: 3rem;
+     margin-bottom: 5rem;
+     margin-left: 2rem;
+
+
+     @media screen and (min-width: 600px) {
+
+        /* justify-content: center; */  
+        
+     }
+`;
+
+
+
+export const StyledList = styled(List)`
+  width: 100%;
+  width: 400px;
+  background-color: white;
+
+  @media screen and (min-width: 600px) {
+    width: 100%;
+    height: 100px;
+  }
 `;
 
 export const EpisodeBold = styled.span`
