d5f6f67e4535b05049a93ba6130617a78930c3fb
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-03-16 21:22:00 +0100

    refractor: try/catch storage.ts

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,12 +1,23 @@
+/**
+ * *getAuthenticatedToken*
+ * *Function for get token. Unless you have token, you won't be able to access to the app*
+ */
 export const getAuthenticatedToken = () => {
      try {
           const token = window.localStorage.getItem('token');
           return token ? JSON.parse(token) : null;
-     } catch (error) {
-          console.log(error);
+     } catch (error: any) {
+          return error.message;
      }
 };
-
+/**
+ * *setAuthenticatedToken*
+ * *Function for set token. Unless you have token, you won't be able to access to the app*
+ */
 export const setAuthenticatedToken = (token: string) => {
-     window.localStorage.setItem('token', JSON.stringify(token));
+     try {
+          window.localStorage.setItem('token', JSON.stringify(token));
+     } catch (error: any) {
+          return error.message; 
+     }
 };
